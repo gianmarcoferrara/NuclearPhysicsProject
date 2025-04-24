828d1f14fcc1044271ef199da220d5d381f81c2a
gianmarcoferrara <gianmarco.ferrara2@studio.unibo.it>
2025-04-24 18:25:17 +0200

    format

--- renormalize_schrodinger.py
+++ renormalize_schrodinger.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
 #Radial Schrodinger equation using Numerov method
 
 import numpy as np
@@ -11,9 +17,9 @@ def prepare_grid(n_points, x_min, dx):
 
     #preparing x-array with constant step
     x = np.linspace(x_min, x_min + ((n_points - 1) * dx), n_points)
-    x = np.append(x, x[n_points - 1] + dx) #another point for convenience of indices
+    x = np.append(x, x[n_points - 1] + dx) #another point for convenient labelling
 
-    #generate r, sqrt_r, and r^2 based on logarithmic x grid
+    #generate r, sqrt_r, and r^2 (logarithmic grid)
     r = np.exp(x)
     sqrt_r = np.sqrt(r)
     r2 = np.power(r, 2)
@@ -30,6 +36,7 @@ def prepare_grid(n_points, x_min, dx):
     return r, sqrt_r, r2
 
 
+# In[25]:
 
 
 def init_potential_coulomb(r):
@@ -83,6 +90,7 @@ def init_potential_short(r):
     return v_pot
 
 
+# In[26]:
 
 
 def solve_schrodinger(n_points, dx, v_pot, r2, r, sqrt_r, n, l):
@@ -174,9 +182,7 @@ def solve_schrodinger(n_points, dx, v_pot, r2, r, sqrt_r, n, l):
             #correct number of nodes, we can perform inward integration.
 
             #determination of the wavefunction in last two points
-            #assuming y[n_points + 1] = 0 and y[n_points] = dx
-            #y[n_points] = dx
-            #y[n_points - 1] = (12. - f[n_points] * 10.) * y[n_points] / f[n_points - 1]
+            #assuming
             y[n_points] = 0
             y[n_points - 1] = dx
 
@@ -204,6 +210,7 @@ def solve_schrodinger(n_points, dx, v_pot, r2, r, sqrt_r, n, l):
             for j in range(0, n_points + 1):
                 y[j] /= norm
 
+            #perturbation theory
             #find the value of the cusp at the matching point
             j = class_inv
             y_cusp = (y[j - 1] * f[j - 1] + f[j + 1] * y[j + 1] + f[j] * 10. * y[j]) / 12.
@@ -238,13 +245,14 @@ def solve_schrodinger(n_points, dx, v_pot, r2, r, sqrt_r, n, l):
     #convergence achived
     #print(f"convergence for n = {n} achived at iter # {i:4d}, de = {de:16.8e}, e = {e:16.8e}\n")
 
-    #compute phase shift ar r=100
+    #compute phase shift ar r = 100
     i_100 = 1261 #index corresponding to r = 100
     phase_shift = np.arcsin(y[i_100]) - np.sqrt(np.absolute(e)) * r[i_100] + (l * np.pi)/2
 
     return e, y, phase_shift
 
 
+# In[27]:
 
 
 def analysis(n_max, pot_name, e_n, p_n, y_n, n_points, dx, v_pot, r2 , r, sqrt_r):
@@ -289,13 +297,14 @@ def analysis(n_max, pot_name, e_n, p_n, y_n, n_points, dx, v_pot, r2 , r, sqrt_r
     return e_n, y_n, p_n
 
 
+# In[28]:
 
 
 #grid parameters
 r_max = 400
 x_min = -8. #corresponds to r_min == 3 * 1E-4 Bohr radii
 dx = 0.01 #grid spacing
-n_max = 10 # maximum energy level (# of nodes limited by range)
+n_max = 10 #maximum energy level (# of nodes limited by range)
 
 #number of points of the grid
 n_points = int((np.log(r_max) - x_min) / dx)
@@ -312,14 +321,25 @@ e_coulomb = np.zeros(n_max)
 y_coulomb = np.zeros((n_max, n_points + 1))
 ph_shift_coulomb = np.zeros(n_max)
 
-e_coulomb, y_coulomb, ph_shift_coulomb = analysis(n_max, v_pot_1_name, e_coulomb, ph_shift_coulomb, y_coulomb, n_points, dx, v_pot_1, r2 , r, sqrt_r)
+e_coulomb, y_coulomb, ph_shift_coulomb = analysis(
+    n_max, v_pot_1_name, e_coulomb, ph_shift_coulomb, y_coulomb,
+    n_points, dx, v_pot_1, r2, r, sqrt_r
+)
 
 #comparison with analytical values -1/n^2
 e_theo = np.arange(1, n_max + 1)
 e_theo = -1 / (e_theo ** 2)
 
 plt.figure()
-plt.plot(np.absolute(e_theo), np.absolute(e_coulomb - e_theo)/ np.absolute(e_theo) , marker = 'o', linestyle = '--', label = "relative error coulomb", color = 'blue')
+plt.plot(
+    np.absolute(e_theo),
+    np.absolute(e_coulomb - e_theo) / np.absolute(e_theo),
+    marker='o',
+    linestyle='--',
+    label="relative error coulomb",
+    color='blue'
+)
+
 plt.xlabel(r'$|E|$')
 plt.ylabel(r'$|\Delta E / E|$')
 plt.title(f"relative error (with analytical values) for energy eigenvalues")
@@ -332,6 +352,7 @@ plt.show()
 plt.close()
 
 
+# In[29]:
 
 
 #initialize potential
@@ -343,9 +364,13 @@ e_short = np.zeros(n_max)
 y_short = np.zeros((n_max, n_points + 1))
 ph_shift_short = np.zeros(n_max)
 
-e_short, y_short, ph_shift_short = analysis(n_max, v_pot_2_name, e_short, ph_shift_short, y_short, n_points, dx, v_pot_2, r2 , r, sqrt_r)
+e_short, y_short, ph_shift_short = analysis(
+    n_max, v_pot_2_name, e_short, ph_shift_short, y_short,
+    n_points, dx, v_pot_2, r2, r, sqrt_r
+)
 
 
+# In[30]:
 
 
 #lowest-energy data is the one corresponing to n_max = 10
@@ -354,6 +379,7 @@ c = np.sqrt(np.pi) * (e_short[n_max - 1] * np.power(n_max, 3) + n_max)
 print("c = ", c)
 
 
+# In[31]:
 
 
 #coulomb eigenvalues in e_theo
@@ -364,8 +390,24 @@ e_app = - 1/ np.power(e_app, 2) + c / (np.sqrt(np.pi) * np.power(e_app, 3))
 
 #plotting relative errors
 plt.figure()
-plt.plot(np.absolute(e_short), np.absolute(e_short - e_theo)/ np.absolute(e_short) , marker = 'o', linestyle = '--', label = "coulomb", color = 'red')
-plt.plot(np.absolute(e_short), np.absolute(e_short - e_app)/ np.absolute(e_short) , marker = 'o', linestyle = '--', label = "delta, first-order", color = 'blue')
+plt.plot(
+    np.absolute(e_short),
+    np.absolute(e_short - e_theo) / np.absolute(e_short),
+    marker='o',
+    linestyle='--',
+    label="coulomb",
+    color='red'
+)
+
+plt.plot(
+    np.absolute(e_short),
+    np.absolute(e_short - e_app) / np.absolute(e_short),
+    marker='o',
+    linestyle='--',
+    label="delta, first-order",
+    color='blue'
+)
+
 plt.xlabel(r'$|E|$')
 plt.ylabel(r'$|\Delta E / E|$')
 plt.xscale("log")
@@ -378,12 +420,20 @@ plt.show()
 plt.close()
 
 
+# In[32]:
 
 
 def init_potential_effective(r, a, c, d_1):
 
-    #definition of the potential
-    v_pot = -2/r * erf(r / (np.sqrt(2) * a)) + c * np.power(a,2) * np.exp(-np.power(r,2) / (2 * np.power(a,2))) / (np.power(2 * np.pi, 1.5) * np.power(a,3)) + d_1 * np.power(a,4) * ((np.power(r,2) / np.power(a,2)) - 3) * np.exp(-np.power(r,2) / (2 * np.power(a,2))) / (np.power(2 * np.pi, 1.5) * np.power(a,5)) 
+    #definition of the potential 
+    v_pot = (
+    -2 / r * erf(r / (np.sqrt(2) * a))
+    + c * np.power(a, 2) * np.exp(-np.power(r, 2) / (2 * np.power(a, 2)))
+      / (np.power(2 * np.pi, 1.5) * np.power(a, 3))
+    + d_1 * np.power(a, 4) * ((np.power(r, 2) / np.power(a, 2)) - 3)
+      * np.exp(-np.power(r, 2) / (2 * np.power(a, 2)))
+      / (np.power(2 * np.pi, 1.5) * np.power(a, 5))
+    )
 
     #saving the potential to CSV
     df_pot = pd.DataFrame({"r": r, "V(r)": v_pot})
@@ -410,6 +460,7 @@ def init_potential_effective(r, a, c, d_1):
     return v_pot
 
 
+# In[33]:
 
 
 a = [1,2,6,10] #cutoff
@@ -446,13 +497,25 @@ e_eff_a4_cut1 = np.zeros(n_max)
 
 for a_value, color in zip(a, colors):
     #compute parameter
-    result1 = minimize(cost_function_1, c_a2, method='nelder-mead', args=(a_value, n_points, dx, r2, r, sqrt_r, n_max), options={'maxiter': 10000,'maxfev': 10000,'xatol': 1E-8, 'disp': True})
-    result2 = minimize(cost_function_2, init_parameters, method='nelder-mead', args=(a_value, n_points, dx, r2, r, sqrt_r, n_max), options={'maxiter': 10000,'maxfev': 10000,'xatol': 1E-8, 'disp': True})
+
+    result1 = minimize(
+    cost_function_1, c_a2, method='nelder-mead',
+    args=(a_value, n_points, dx, r2, r, sqrt_r, n_max),
+    options={'maxiter': 10000, 'maxfev': 10000, 'xatol': 1E-8, 'disp': True}
+    )
+
+    result2 = minimize(
+    cost_function_2, init_parameters, method='nelder-mead',
+    args=(a_value, n_points, dx, r2, r, sqrt_r, n_max),
+    options={'maxiter': 10000, 'maxfev': 10000, 'xatol': 1E-8, 'disp': True}
+    )
 
     c_a2 = result1.x
+    
     parameters_a4 = result2.x
     c_a4 = parameters_a4[0]
     d_1 = parameters_a4[1]
+    
     print(f"----------------------|a = {a_value}|----------------------\n")
     print(f"(a^2 theory):    c = {c_a2} \n")
     print(f"(a^4 theory): c = {c_a4}, d_1 = {d_1}\n")
@@ -471,10 +534,18 @@ for a_value, color in zip(a, colors):
     y_eff_a4 = np.zeros((n_max, n_points + 1))
     ph_shift_eff_a4 = np.zeros(n_max)
 
-    e_eff_a2, y_eff_a2, ph_shift_eff_a2 = analysis(n_max, v_pot_3_name, e_eff_a2, ph_shift_eff_a2, y_eff_a2, n_points, dx, v_pot_3, r2 , r, sqrt_r)
-    e_eff_a4, y_eff_a4, ph_shift_eff_a4 = analysis(n_max, v_pot_4_name, e_eff_a4, ph_shift_eff_a4, y_eff_a4, n_points, dx, v_pot_4, r2 , r, sqrt_r)
+    #analysis
+    e_eff_a2, y_eff_a2, ph_shift_eff_a2 = analysis(
+    n_max, v_pot_3_name, e_eff_a2, ph_shift_eff_a2, y_eff_a2,
+    n_points, dx, v_pot_3, r2, r, sqrt_r
+    )
 
-    #out of this scope we want to use again results for a=1
+    e_eff_a4, y_eff_a4, ph_shift_eff_a4 = analysis(
+    n_max, v_pot_4_name, e_eff_a4, ph_shift_eff_a4, y_eff_a4,
+    n_points, dx, v_pot_4, r2, r, sqrt_r
+    )
+
+    #out of this scope we want to use again results for a=1, so, save them
     if a_value == 1:
         e_eff_a2_cut1 = e_eff_a2
         e_eff_a4_cut1 = e_eff_a4
@@ -482,7 +553,15 @@ for a_value, color in zip(a, colors):
     #plotting relative errors for phase shifts and enrgies
     #energy
     plt.figure(1)
-    plt.plot(np.absolute(e_short), np.absolute(e_short - e_eff_a2)/ np.absolute(e_short) , marker = 'o', linestyle = '--', label = f"a = {a_value}", color = color)
+    plt.plot(
+    np.absolute(e_short),
+    np.absolute(e_short - e_eff_a2) / np.absolute(e_short),
+    marker='o',
+    linestyle='--',
+    label=f"a = {a_value}",
+    color=color
+    )
+
     plt.xlabel(r'$|E|$')
     plt.ylabel(r'$|\Delta E / E|$')
     plt.xscale("log")
@@ -494,7 +573,15 @@ for a_value, color in zip(a, colors):
         plt.savefig(f"relative_errors/relative_error_energy_a2.pdf")
 
     plt.figure(2)
-    plt.plot(np.absolute(e_short), np.absolute(e_short - e_eff_a4)/ np.absolute(e_short) , marker = 'o', linestyle = '--', label = f"a = {a_value}", color = color)
+    plt.plot(
+    np.absolute(e_short),
+    np.absolute(e_short - e_eff_a4) / np.absolute(e_short),
+    marker='o',
+    linestyle='--',
+    label=f"a = {a_value}",
+    color=color
+    )
+
     plt.xlabel(r'$|E|$')
     plt.ylabel(r'$|\Delta E / E|$')
     plt.xscale("log")
@@ -507,7 +594,15 @@ for a_value, color in zip(a, colors):
     
     #phase_shift
     plt.figure(3)
-    plt.plot(np.absolute(e_short), np.absolute(ph_shift_short - ph_shift_eff_a2) , marker = 'o', linestyle = '--', label = f"a = {a_value}", color = color)
+    plt.plot(
+    np.absolute(e_short),
+    np.absolute(ph_shift_short - ph_shift_eff_a2),
+    marker='o',
+    linestyle='--',
+    label=f"a = {a_value}",
+    color=color
+    )
+
     plt.xlabel(r'$|E|$')
     plt.ylabel(r'$|\Delta \delta(E)|$')
     plt.xscale("log")
@@ -518,7 +613,15 @@ for a_value, color in zip(a, colors):
         plt.savefig(f"relative_errors/relative_error_ph_shift_a2.pdf")
 
     plt.figure(4)
-    plt.plot(np.absolute(e_short), np.absolute(ph_shift_short - ph_shift_eff_a4) , marker = 'o', linestyle = '--', label = f"a = {a_value}", color = color)
+    plt.plot(
+    np.absolute(e_short),
+    np.absolute(ph_shift_short - ph_shift_eff_a4),
+    marker='o',
+    linestyle='--',
+    label=f"a = {a_value}",
+    color=color
+    )
+
     plt.xlabel(r'$|E|$')
     plt.ylabel(r'$|\Delta \delta(E)|$')
     plt.xscale("log")
@@ -529,12 +632,34 @@ for a_value, color in zip(a, colors):
         plt.savefig(f"relative_errors/relative_error_ph_shift_a4.pdf")        
 
 
+# In[34]:
 
 
 plt.figure()
-plt.plot(np.absolute(e_short), np.absolute(e_short - e_theo)/ np.absolute(e_short) , marker = 'o', linestyle = '--', label = f"coulomb", color = 'black')
-plt.plot(np.absolute(e_short), np.absolute(e_short - e_app)/ np.absolute(e_short) , marker = 'o', linestyle = '--', label = f"delta - perturbation", color = 'blue')
-plt.plot(np.absolute(e_short), np.absolute(e_short - e_eff_a4_cut1)/ np.absolute(e_short) , marker = 'o', linestyle = '--', label = f"a^4 theory", color = 'red')
+plt.plot(
+    np.absolute(e_short),
+    np.absolute(e_short - e_theo) / np.absolute(e_short),
+    marker='o',
+    linestyle='--',
+    label='coulomb',
+    color='black'
+)
+plt.plot(
+    np.absolute(e_short),
+    np.absolute(e_short - e_app) / np.absolute(e_short),
+    marker='o',
+    linestyle='--',
+    label='delta - perturbation',
+    color='blue'
+)
+plt.plot(
+    np.absolute(e_short),
+    np.absolute(e_short - e_eff_a4_cut1) / np.absolute(e_short),
+    marker='o',
+    linestyle='--',
+    label='a^4 theory',
+    color='red'
+)
 plt.xlabel(r'$|E|$')
 plt.ylabel(r'$|\Delta E / E|$')
 plt.xscale("log")
